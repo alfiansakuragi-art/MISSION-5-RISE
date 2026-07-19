9d994dcbf136ce77a3b720753f323af32e5c148b
alfian <alfiansakuragi@gmail.com>
2026-07-19 20:28:50 +0800

    fix bug

--- src/component/BaseComponent/Header.jsx
+++ src/component/BaseComponent/Header.jsx
@@ -10,9 +10,9 @@ function Header({setIsOpen}) {
     <header>
         <img src={logo} alt="" />
         <nav>
-            <a onClick={() => {navigate("")}} href="/">Home</a>
-            <a onClick={() => {navigate("")}} href="/kategory">Kategory</a>
-            <button onClick={() => {navigate("/auth?mode=login")}} id="login"><a href="#">Login</a></button>
+            <a onClick={() => {navigate("/")}} href="">Home</a>
+            <a onClick={() => {navigate("/kategory")}} href="">Kategory</a>
+            <button onClick={() => {navigate("/auth?mode=login")}} id="login"><a>Login</a></button>
             <button onClick={() => {navigate("/auth?mode=register")}} id="register"><a href="#">Register</a></button>
         </nav>
         <span id='sidebar' onClick={toggleSidebar}><ion-icon name="menu-outline"></ion-icon></span>
